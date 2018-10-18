721b3db69a18487d3be11513c20e69d00a9b259a
JEFFREY TEOH <jeffrey_pooi@hotmail.com>
2018-10-18 15:14:55 +0800

    Added all drawing component and added firebase config file

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDAQGdhjdI_LkV3qO1NbguS0Od7xkdKKCM",
+  authDomain: "ccda-bd25f.firebaseapp.com",
+  databaseURL: "https://ccda-bd25f.firebaseio.com",
+  projectId: "ccda-bd25f",
+  storageBucket: "ccda-bd25f.appspot.com",
+  messagingSenderId: "430331119499"
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import * as fb from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor() {
 
   }
 
+  guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
 }
